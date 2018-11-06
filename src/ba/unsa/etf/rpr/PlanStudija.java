686bb860711e41e7709b72da5e2a5b5600781540
RijadSehovic <rsehovic@etf.unsa.ba>
2018-11-06 18:33:37 +0100

    Napravio klasu PlanStudija

--- src/ba/unsa/etf/rpr/PlanStudija.java
+++ src/ba/unsa/etf/rpr/PlanStudija.java
@@ -1,9 +1,21 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PlanStudija {
-    private Map<Integer, List<Predmet>> PredmetiUSemestru;
+    private Map<Integer, List<Predmet>> predmetiUSemestru = new HashMap<>();
+
+    private void dodajPredmetuSemestar(Integer brojSemestra, Predmet predmet){
+
+        List<Predmet> predmets = this.predmetiUSemestru.get(brojSemestra);
+        if(predmets ==null)
+            predmetiUSemestru.put(brojSemestra,new ArrayList<>());
+
+        this.predmetiUSemestru.get(brojSemestra).add(predmet);
+
+    }
 
 }
